2e4ad92c13f94fb74195186ab54f8698aa409921
Maulik Khandelwal <kmaulik07@gmail.com>
2022-04-03 01:53:54 +0530

    Study room added.

---
+++ lib/screens/room/home_page.dart
@@ -0,0 +1,295 @@
+import 'package:flutter/material.dart';
+import 'package:privateroom/screens/room/pmd_page/pmd_page.dart';
+import 'package:privateroom/screens/room/pmd_select/pmd_select_page.dart';
+import 'package:privateroom/screens/room/room.dart';
+import 'package:privateroom/screens/room/time_select/time_select_page.dart';
+import 'package:privateroom/screens/room/timer_page.dart';
+
+class RoomHome extends StatefulWidget {
+  const RoomHome({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return RoomHomeState();
+  }
+}
+
+class RoomHomeState extends State<RoomHome> {
+  List<String> days = ["We", "Th", "Fr", "Sa", "Su"];
+
+  static String dateTimetoHumanReadable(DateTime dt) {
+    String dateSlug =
+        "${dt.year.toString()}-${dt.month.toString().padLeft(2, '0')}-${dt.day.toString().padLeft(2, '0')}";
+    return dateSlug;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/bg.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: ListView(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 32.0, vertical: 12.0),
+            children: [
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Good Afternoon",
+                          style:
+                              Theme.of(context).textTheme.titleLarge?.copyWith(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                        ),
+                        Text(
+                          "Simrat",
+                          style:
+                              Theme.of(context).textTheme.titleLarge?.copyWith(
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  PhysicalModel(
+                    color: Colors.grey,
+                    shape: BoxShape.circle,
+                    elevation: 4,
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundColor: const Color(0xffBDDBEC),
+                      child: Image.asset(
+                        "assets/tomato_red.png",
+                        width: 36,
+                        height: 36,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 40),
+              const SizedBox(
+                height: 20,
+              ),
+              Card(
+                color: const Color(0xffD7DEF8),
+                child: Container(
+                  padding: const EdgeInsets.all(
+                    20.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Today: ${dateTimetoHumanReadable(DateTime.now())}",
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                          Text(
+                            "Your Study Streak: ${days.length} Days",
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      const Divider(),
+                      const SizedBox(
+                        height: 10.0,
+                      ),
+                      Row(
+                        children: days
+                            .map(
+                              (e) => Expanded(
+                                child: CircleAvatar(
+                                  child: Text(e),
+                                ),
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Material(
+                type: MaterialType.transparency,
+                child: Card(
+                  color: const Color(0xff8A93E9),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(12),
+                    onTap: () => {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (_) => const Room(),
+                        ),
+                      )
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(
+                        20.0,
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Begin a group study session",
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleLarge
+                                ?.copyWith(fontSize: 30),
+                          ),
+                          const SizedBox(
+                            height: 40.0,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "Invite Friends",
+                                style: Theme.of(context).textTheme.titleLarge,
+                              ),
+                              const Spacer(),
+                              Row(
+                                children: const [
+                                  Icon(
+                                    Icons.message,
+                                    size: 30,
+                                  ),
+                                  SizedBox(
+                                    width: 12,
+                                  ),
+                                  Icon(
+                                    Icons.mail,
+                                    size: 30,
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Material(
+                type: MaterialType.transparency,
+                child: Card(
+                  color: const Color(0xffFFE0C3),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(14),
+                    onTap: () => {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (_) => TimeSelectPage(
+                            callback: (p) {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => TimerPage(
+                                    timerStateChangeNotifier: p,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      )
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12.0,
+                        vertical: 16.0,
+                      ),
+                      child: Row(
+                        children: [
+                          Text(
+                            "Set a timer block",
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium
+                                ?.copyWith(fontSize: 22),
+                          ),
+                          const Spacer(),
+                          Image.asset(
+                            "assets/hourglass.png",
+                            height: 30,
+                            width: 30,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Material(
+                type: MaterialType.transparency,
+                child: Card(
+                  color: const Color(0xffFFD7D7),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(14),
+                    onTap: () => {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (_) => PmdSelectPage(
+                            callback: (p) {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => PmdPage(
+                                    pmdStateChangeNotifier: p,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      )
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12.0,
+                        vertical: 16.0,
+                      ),
+                      child: Row(
+                        children: [
+                          Text(
+                            "Start a pomodoro",
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium
+                                ?.copyWith(fontSize: 22),
+                          ),
+                          const Spacer(),
+                          Image.asset(
+                            "assets/tomato_black.png",
+                            height: 30,
+                            width: 30,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/room/timer_page.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:privateroom/screens/room/providers/timer_state_provider.dart';
+import 'package:privateroom/screens/room/timer_page/buddy_row.dart';
+import 'package:privateroom/screens/room/timer_page/done_button.dart';
+import 'package:privateroom/screens/room/timer_page/play_pause_button.dart';
+import 'package:privateroom/screens/room/timer_page/quit_button.dart';
+import 'package:privateroom/screens/room/timer_page/timer.dart';
+
+import 'done_page/done_page.dart';
+
+class TimerPage extends ConsumerStatefulWidget {
+  const TimerPage({Key key, this.timerStateChangeNotifier})
+      : super(key: key);
+
+  final ChangeNotifierProvider<TimerStateProvider> timerStateChangeNotifier;
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return TimerPageState();
+  }
+}
+
+class TimerPageState extends ConsumerState<TimerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/bg.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: ListView(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 32.0, vertical: 12.0),
+            children: [
+              Column(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Card(
+                    color: const Color(0xffFFE0C3),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 14.0,
+                        vertical: 16.0,
+                      ),
+                      child: Row(
+                        children: [
+                          Text(
+                            "Study Session ~ ${ref.read(widget.timerStateChangeNotifier).duration.inHours}hr",
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium
+                                ?.copyWith(fontSize: 22),
+                          ),
+                          const Spacer(),
+                          const Icon(
+                            Icons.edit,
+                            size: 30,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  TimerText(
+                    initial: const Duration(
+                      hours: 1,
+                      minutes: 20,
+                      seconds: 42,
+                    ),
+                    timerStateNotifier: widget.timerStateChangeNotifier,
+                  ),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  Card(
+                    color: const Color(0xffFFE0C3),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12.0,
+                        vertical: 16.0,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "4 buddies studying with you",
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium
+                                ?.copyWith(fontSize: 22),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const BuddyRow(),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      DoneButton(onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const DonePage(),
+                          ),
+                        );
+                      }),
+                      PlayPauseButton(
+                        timerStateNotifier: widget.timerStateChangeNotifier,
+                      ),
+                      QuitButton(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) => ClipRRect(
+                              borderRadius: BorderRadius.circular(14),
+                              child: AlertDialog(
+                                backgroundColor: Colors.white,
+                                title: const Text("Its sad to see you go!"),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text("Close"),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).popUntil(
+                                        (route) => route.isFirst,
+                                      );
+                                    },
+                                    child: const Text("Exit"),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
